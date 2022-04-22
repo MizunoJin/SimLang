99801daaf533ab7a5d9b0213a199e4f5e7ae5e77
JinMizuno <bump.lk_k.1215@icloud.com>
2022-04-22 14:19:41 +0900

    Remove: remove factorybot from question spec

--- api/spec/models/question_spec.rb
+++ api/spec/models/question_spec.rb
@@ -2,27 +2,27 @@ require 'rails_helper'
 
 RSpec.describe Question, type: :model do
   it "is valid with title and body" do
-    question = FactoryBot.build(:question)
+    question = build(:question)
     expect(question).to be_valid
   end
 
   it "is invalid without title" do
-    question = FactoryBot.build(:question, title: nil)
+    question = build(:question, title: nil)
     expect(question).to be_invalid
   end
 
   it "is invalid without body" do
-    question = FactoryBot.build(:question, body: nil)
+    question = build(:question, body: nil)
     expect(question).to be_invalid
   end
 
   it "is invalid with too long title" do
-    question = FactoryBot.build(:question, title: "a" * 101)
+    question = build(:question, title: "a" * 101)
     expect(question).to be_invalid
   end
 
   it "is invalid with too long body" do
-    question = FactoryBot.build(:question, body: "a" * 1001)
+    question = build(:question, body: "a" * 1001)
     expect(question).to be_invalid
   end
 end
