45a0045af0ce02948e907347b5f9859ed72a53d0
JinMizuno <bump.lk_k.1215@icloud.com>
2022-04-22 17:58:00 +0900

    Add: add request spec to questions controller

--- api/spec/requests/questions_spec.rb
+++ api/spec/requests/questions_spec.rb
@@ -3,7 +3,19 @@
 require 'rails_helper'
 
 RSpec.describe 'Questions', type: :request do
+  let!(:question) { create(:question) }
+
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    it 'returns 200 response' do
+      get questions_path
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'SHOW /:id' do
+    it 'returns 200 response' do
+      get question_path(question)
+      expect(response).to have_http_status(200)
+    end
   end
 end
