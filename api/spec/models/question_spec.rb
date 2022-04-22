a406e14f31f0e7a12f73346f39e9c28a1c3f0d05
JinMizuno <bump.lk_k.1215@icloud.com>
2022-04-22 16:50:41 +0900

    Add: add rubocop to gemfile and run bundle install

--- api/spec/models/question_spec.rb
+++ api/spec/models/question_spec.rb
@@ -1,28 +1,30 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  it "is valid with title and body" do
+  it 'is valid with title and body' do
     question = build(:question)
     expect(question).to be_valid
   end
 
-  it "is invalid without title" do
+  it 'is invalid without title' do
     question = build(:question, title: nil)
     expect(question).to be_invalid
   end
 
-  it "is invalid without body" do
+  it 'is invalid without body' do
     question = build(:question, body: nil)
     expect(question).to be_invalid
   end
 
-  it "is invalid with too long title" do
-    question = build(:question, title: "a" * 101)
+  it 'is invalid with too long title' do
+    question = build(:question, title: 'a' * 101)
     expect(question).to be_invalid
   end
 
-  it "is invalid with too long body" do
-    question = build(:question, body: "a" * 1001)
+  it 'is invalid with too long body' do
+    question = build(:question, body: 'a' * 1001)
     expect(question).to be_invalid
   end
 end
