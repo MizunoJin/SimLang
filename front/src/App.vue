e16b5454d7ca27a358cd8e6ce56625f2ed9466b8
JinMizuno <bump.lk_k.1215@icloud.com>
2022-04-22 20:18:02 +0900

    Add: add question list component

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,15 +1,20 @@
 <template>
-  <HelloWorld />
+  <div id="app">
+    <HelloWorld />
+    <QuestionList />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import HelloWorld from "./components/HelloWorld.vue";
+import QuestionList from "./components/QuestionList.vue";
 
 export default defineComponent({
   name: "App",
   components: {
     HelloWorld,
+    QuestionList,
   },
 });
 </script>
---
+++ front/src/components/QuestionList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Questions</h1>
+    <p>questions:</p>
+    <p>{{ questions }}</p>
+    <button type="submit">fetchquestions</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "QuestionList",
+  data: () => ({
+    questions: [],
+  }),
+};
+</script>
