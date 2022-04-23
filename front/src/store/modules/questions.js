b9b0858e8b1c34ca0a30422bfa5b7c67fb7e2ad3
JinMizuno <bump.lk_k.1215@icloud.com>
2022-04-23 19:41:59 +0900

    Change: change question view layout

--- front/src/store/modules/questions.js
+++ front/src/store/modules/questions.js
@@ -4,20 +4,27 @@ const apiUrl = "http://localhost:3000";
 
 const state = {
   questions: [],
+  question: null,
 };
 
 const getters = {
   questions: (state) => state.questions,
+  question: (state) => state.question,
 };
 
 const mutations = {
   setQuestions: (state, questions) => (state.questions = questions),
+  setQuestion: (state, question) => (state.question = question),
 };
 
 const actions = {
   async fetchQuestions({ commit }) {
     const response = await axios.get(`${apiUrl}/questions`);
     commit("setQuestions", response.data);
+  },
+  async fetchQuestion({ commit }, id) {
+    const response = await axios.get(`${apiUrl}/questions/${id}`);
+    commit("setQuestion", response.data);
   },
 };
 
--- front/src/views/QuestionView.vue
+++ front/src/views/QuestionView.vue
@@ -2,21 +2,42 @@
   <v-container>
     <v-row class="text-center">
       <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">Welcome to SimLang</h1>
+        <h1 class="display-2 font-weight-bold mb-3">{{ question.title }}</h1>
+        <br />
+        {{ question.body }}
 
-        <p class="subheading font-weight-regular">QuestionView</p>
-        <v-btn color="primary" elevation="2">
-          <router-link to="/questions"
-            ><div class="text-h6 white--text">始める</div></router-link
-          >
-        </v-btn>
+        <v-col>
+          <v-textarea
+            name="input-japanese"
+            label="日本語で返答する"
+            hint="日本語で入力してください"
+          ></v-textarea>
+          <v-textarea
+            name="input-foreign"
+            label="外国語で返答する"
+            hint="外国語で入力してください"
+          ></v-textarea>
+          <v-btn color="accent" elevation="6" large>回答する</v-btn>
+        </v-col>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
+import { mapGetters, mapActions } from "vuex";
+
 export default {
-  name: "HomeView",
+  name: "QuestionView",
+
+  computed: {
+    ...mapGetters("questions", ["question"]),
+  },
+  methods: {
+    ...mapActions("questions", ["fetchQuestion"]),
+  },
+  mounted() {
+    this.fetchQuestion(this.$route.params.id);
+  },
 };
 </script>
