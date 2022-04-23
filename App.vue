aa878bd01bd388cca0bd0bc60a3867145458712c
JinMizuno <bump.lk_k.1215@icloud.com>
2022-04-23 14:43:36 +0900

    Add: add vue router

---
+++ App.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app>
+    <v-main>
+      <QuestionList />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import QuestionList from './components/QuestionList'
+
+export default {
+  name: 'App',
+  components: {
+    QuestionList
+  }
+};
+</script>
--- front/src/App.vue
+++ front/src/App.vue
@@ -1,18 +1,32 @@
 <template>
-  <v-app>
-    <v-main>
-      <QuestionList />
-    </v-main>
-  </v-app>
+  <div id="app">
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view />
+  </div>
 </template>
 
-<script>
-import QuestionList from './components/QuestionList'
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-export default {
-  name: 'App',
-  components: {
-    QuestionList
-  }
-};
-</script>
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
