c66e18c9898512d3e4b0fecd8d02bbf7f348cde2
JinMizuno <bump.lk_k.1215@icloud.com>
2022-04-23 11:32:50 +0900

    Add: add axios

---
+++ front/src/components/QuestionList.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container>
+    <h1 class="text-h1">QuestionList</h1>
+    <v-list>
+      <v-list-item v-for="question in questions" :key="question.id">
+        {{ question.title }}
+      </v-list-item>
+    </v-list>
+    <v-btn type="submit" @click="fetchQuestions()">fetchQuestions</v-btn>
+  </v-container>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: "QuestionList",
+
+  data: () => ({
+    questions: []
+  }),
+  methods: {
+    fetchQuestions() {
+      axios
+        .get('http://localhost:3000/questions')
+        .then(response => {
+          this.questions = response.data;
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+  }
+};
+</script>
