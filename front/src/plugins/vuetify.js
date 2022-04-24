30b49b13606897766f9b085927aec46663eecbbd
JinMizuno <bump.lk_k.1215@icloud.com>
2022-04-24 19:33:14 +0900

    Add: loading button

--- front/src/plugins/vuetify.js
+++ front/src/plugins/vuetify.js
@@ -1,6 +1,18 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib/framework";
+import colors from "vuetify/lib/util/colors";
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: colors.lightBlue.lighten1,
+        secondary: colors.cyan.lighten4,
+        accent: colors.orange,
+        error: colors.red.lighten4,
+      },
+    },
+  },
+});
