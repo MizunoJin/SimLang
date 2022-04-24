66ba1c770c5cdb2766d4d4cf3f0d8f24bd700343
JinMizuno <bump.lk_k.1215@icloud.com>
2022-04-24 17:00:57 +0900

    Refactor: change name from set to fetch

--- api/app/models/api/deepl.rb
+++ api/app/models/api/deepl.rb
@@ -1,38 +1,44 @@
-class Api::Deepl
-  include ActiveModel::Model
-  require 'net/https'
-  require 'uri'
-  require 'json'
+# frozen_string_literal: true
 
-  attr_accessor :text, :target_lang
+module Api
+  class Deepl
+    include ActiveModel::Model
+    require 'net/https'
+    require 'uri'
+    require 'json'
 
-  validates :text, presence: true
-  validates :target_lang, presence: true
+    attr_accessor :text, :target_lang
 
-  def translate
-    uri = set_url(text: text, target_lang: target_lang)
-    http = set_http(uri: uri)
-    get_translation(uri: uri, http: http)
-  end
+    validates :text, presence: true
+    validates :target_lang, presence: true
 
-  private
-  def set_url(text: , target_lang: )
-    uri = URI.parse('https://api-free.deepl.com/v2/translate')
-    uri.query = URI.encode_www_form({ auth_key: Rails.application.credentials.deepl[:auth_key], text: text, target_lang: target_lang })
-    uri
-  end
+    def translate
+      uri = set_url(text: text, target_lang: target_lang)
+      http = fetch_http(uri: uri)
+      fetch_translation(uri: uri, http: http)
+    end
 
-  def set_http(uri:)
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    http
-  end
+    private
 
-  def get_translation(uri:, http:)
-    request = Net::HTTP::Get.new(uri.request_uri)
-    response = http.request(request)
-    result = JSON.parse(response.body, { symbolize_names: true })
-    result[:translations].first
+    def set_url(text:, target_lang:)
+      uri = URI.parse('https://api-free.deepl.com/v2/translate')
+      uri.query = URI.encode_www_form({ auth_key: Rails.application.credentials.deepl[:auth_key], text: text,
+                                        target_lang: target_lang })
+      uri
+    end
+
+    def fetch_http(uri:)
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      http
+    end
+
+    def fetch_translation(uri:, http:)
+      request = Net::HTTP::Get.new(uri.request_uri)
+      response = http.request(request)
+      result = JSON.parse(response.body, { symbolize_names: true })
+      result[:translations].first
+    end
   end
 end
