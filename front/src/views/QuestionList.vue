ab6a51471679e0326035bf1d2c98482f4e3fbed2
JinMizuno <bump.lk_k.1215@icloud.com>
2022-04-24 19:47:09 +0900

    Add: add card list

--- front/src/views/QuestionList.vue
+++ front/src/views/QuestionList.vue
@@ -1,11 +1,49 @@
 <template>
   <v-container>
-    <h1 class="text-h2">お好きな問題を選んでください</h1>
-    <v-list>
+    <v-list class="d-flex mt-15">
       <v-list-item v-for="question in questions" :key="question.id">
-        <router-link :to="{ name: 'question', params: { id: question.id } }">
-          {{ question.title }}
-        </router-link>
+        <v-card class="mx-auto" max-width="344">
+          <v-img
+            src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+            height="200px"
+          ></v-img>
+
+          <v-card-title>{{ question.title }}</v-card-title>
+
+          <v-card-subtitle> 1,000 miles of wonder </v-card-subtitle>
+
+          <v-card-actions>
+            <v-btn
+              color="accent"
+              :to="{ name: 'question', params: { id: question.id } }"
+            >
+              挑戦する
+            </v-btn>
+
+            <v-spacer></v-spacer>
+
+            <v-btn icon @click="show = !show">
+              <v-icon>{{
+                show ? "mdi-chevron-up" : "mdi-chevron-down"
+              }}</v-icon>
+            </v-btn>
+          </v-card-actions>
+
+          <v-expand-transition>
+            <div v-show="show">
+              <v-divider></v-divider>
+
+              <v-card-text>
+                I'm a thing. But, like most politicians, he promised more than
+                he could deliver. You won't have time for sleeping, soldier, not
+                with all the bed making you'll be doing. Then we'll go with that
+                data file! Hey, you add a one and two zeros to that or we walk!
+                You're going to do his laundry? I've got to find a way to
+                escape.
+              </v-card-text>
+            </div>
+          </v-expand-transition>
+        </v-card>
       </v-list-item>
     </v-list>
   </v-container>
