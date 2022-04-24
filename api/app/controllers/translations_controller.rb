bcbba21e7f0a4699c9415b569f41a6eb9ba8a14e
JinMizuno <bump.lk_k.1215@icloud.com>
2022-04-24 15:48:35 +0900

    Fix: fix rubocop errors

--- api/app/controllers/translations_controller.rb
+++ api/app/controllers/translations_controller.rb
@@ -1,19 +1,22 @@
+# frozen_string_literal: true
+
 class TranslationsController < ApplicationController
   require 'net/https'
   require 'uri'
   require 'json'
 
   def translate
-    uri = URI.parse("https://api-free.deepl.com/v2/translate")
-    uri.query = URI.encode_www_form({ auth_key: Rails.application.credentials.deepl[:auth_key], text: params[:text], target_lang: params[:target_lang] })
+    uri = URI.parse('https://api-free.deepl.com/v2/translate')
+    uri.query = URI.encode_www_form({ auth_key: Rails.application.credentials.deepl[:auth_key], text: params[:text],
+                                      target_lang: params[:target_lang] })
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
     request = Net::HTTP::Get.new(uri.request_uri)
     response = http.request(request)
-    result = JSON.parse(response.body, {symbolize_names: true})
-    translation = result.dig(:translations).first
+    result = JSON.parse(response.body, { symbolize_names: true })
+    translation = result[:translations].first
 
     render json: translation
   end
