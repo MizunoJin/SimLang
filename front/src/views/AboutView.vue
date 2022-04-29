da253d676e7ba9fba7b3d1383486ae90425e0073
JinMizuno <bump.lk_k.1215@icloud.com>
2022-04-30 08:47:00 +0900

    Fix: fix about page

--- front/src/views/AboutView.vue
+++ front/src/views/AboutView.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-container>
+    <v-row class="text-center">
+      <v-col class="mb-4">
+        <p class="subheading font-weight-regular">
+          SimLangはAIで他言語を学習できるサービスです。
+        </p>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
