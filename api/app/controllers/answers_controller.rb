d020770e3e5af24425464d694269a847459fb47f
JinMizuno <bump.lk_k.1215@icloud.com>
2022-05-01 16:54:16 +0900

    Add: add textgears API

--- api/app/controllers/answers_controller.rb
+++ api/app/controllers/answers_controller.rb
@@ -2,7 +2,22 @@
 
 class AnswersController < ApplicationController
   def fetch_answer
-    deepl = Api::Deepl.new(text: params[:text], target_lang: params[:target_lang])
+    require 'uri'
+    require 'net/http'
+    require 'json'
+
+    url = URI.parse("https://api.textgears.com/grammar")
+    url.query = URI.encode_www_form({ key: Rails.application.credentials.textgears[:auth_key], text: params[:foreign_text], target_lang: params[:target_lang] })
+
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = Net::HTTP::Post.new(url)
+    response = http.request(request)
+    result = JSON.parse(response.body, { symbolize_names: true })
+
+    deepl = Api::Deepl.new(text: params[:japanese_text], target_lang: params[:target_lang])
     translation = deepl.translate
     render json: translation
   end
