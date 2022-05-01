2ec9f47eecb2b19748fd570acef041e9e6e44bde
JinMizuno <bump.lk_k.1215@icloud.com>
2022-05-01 18:04:36 +0900

    Refactor: add textgears model

--- api/app/controllers/answers_controller.rb
+++ api/app/controllers/answers_controller.rb
@@ -2,20 +2,8 @@
 
 class AnswersController < ApplicationController
   def fetch_answer
-    require 'uri'
-    require 'net/http'
-    require 'json'
-
-    url = URI.parse("https://api.textgears.com/grammar")
-    url.query = URI.encode_www_form({ key: Rails.application.credentials.textgears[:auth_key], text: params[:foreign_text], target_lang: params[:target_lang] })
-
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-    request = Net::HTTP::Post.new(url)
-    response = http.request(request)
-    result = JSON.parse(response.body, { symbolize_names: true })
+    textgears = Api::Textgears.new(text: params[:foreign_text], language: params[:target_lang])
+    check = textgears.check
 
     deepl = Api::Deepl.new(text: params[:japanese_text], target_lang: params[:target_lang])
     translation = deepl.translate
---
+++ api/app/models/api/textgears.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+module Api
+  class Textgears
+    include ActiveModel::Model
+    require 'net/https'
+    require 'uri'
+    require 'json'
+
+    attr_accessor :text, :language
+
+    validates :text, presence: true
+    validates :language, presence: true
+
+    def check
+      uri = set_url(text: text, language: language)
+      http = fetch_http(uri: uri)
+      fetch_data(uri: uri, http: http)
+    end
+
+    private
+
+    def set_url(text:, language:)
+      uri = URI.parse("https://api.textgears.com/grammar")
+      uri.query = URI.encode_www_form({ key: Rails.application.credentials.textgears[:auth_key], text: text,
+                                        language: "es-ES" })
+      uri
+    end
+
+    def fetch_http(uri:)
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      http
+    end
+
+    def fetch_data(uri:, http:)
+      request = Net::HTTP::Get.new(uri.request_uri)
+      response = http.request(request)
+      result = JSON.parse(response.body, { symbolize_names: true })
+      result
+    end
+  end
+end
