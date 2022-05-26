5e5acca5e51c735eb803b69e20dbb1c778b5b642
JinMizuno <bump.lk_k.1215@icloud.com>
2022-05-26 21:18:49 +0900

    Add: User model and jwt denylist model

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   resources :categories, only: %i[index show]
   resources :questions, only: %i[index show]
   get '/fetch_answer', to: 'answers#fetch_answer'
